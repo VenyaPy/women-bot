71c2a1cdaf979da91767dcf4662981d1c11bec77
venya <ve.po2014@yandex.ru>
2024-06-06 18:00:41 +0700

    First commit for freelance project

---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from config import TOKEN_BOT
+from aiogram.client.default import DefaultBotProperties
+
+TOKEN = TOKEN_BOT
+bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+dp = Dispatcher()
+
+dp.include_routers()
+
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
